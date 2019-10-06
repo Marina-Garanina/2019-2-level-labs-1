2ebcb86fd04275e2d5962e87767224c17623eaa5
Marina-Garanina <55386364+Marina-Garanina@users.noreply.github.com>
2019-10-07 01:28:50 +0300

    laboratory1
    add the code

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,23 +1,43 @@
-"""
-Labour work #1
-Count frequencies dictionary by the given arbitrary text
-"""
+f = open('data.txt', 'r')
+text = f.read().strip()
+text = "".join(word for word in text if word not in ('!','.',':', ',', '“', '”', '"', '?', '?!'))
 
+stopwords = ('the', 'a', 'an', 'or', 'and', 'to', 'are', 'is',)
+top_n = 6
 
-def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
+def calculate_frequences(text) :
+    dictionary = {}
+    words = text.split()
+    for key in words:
+        key = key.lower()
+        if key in dictionary:
+            value = dictionary[key]
+            dictionary[key]=value+1
+        else:
+            dictionary[key]=1
+    return dictionary
+dictionary = calculate_frequences(text)
+print (dictionary)
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
+def filter_stop_words(dictionary, stopwords):
+    filtered_dictionary = dictionary
+    for key in list(filtered_dictionary.keys()):
+        if key in stopwords:
+            del filtered_dictionary[key]
+    return filtered_dictionary
+filtered_dictionary = filter_stop_words(dictionary, stopwords)
+print (filtered_dictionary)
 
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+def get_top_n (dictionary, top_n):
+    list_dictionary = list(dictionary.items())
+    list_dictionary.sort(key=lambda i: i[1], reverse=True)
+    index = 0
+    toped_dictionary = list()
+    for el in list_dictionary:
+        if index < top_n:
+            toped_dictionary.append(list_dictionary[index])
+            index += 1
+    toped_dictionary = tuple(list_dictionary[:top_n])
+    return toped_dictionary
+toped_dictionary = get_top_n (dictionary, top_n)
+print (toped_dictionary)
