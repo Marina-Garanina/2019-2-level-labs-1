8bc2a34bee94c02bfa13aac515317e9098776ba8
Marina-Garanina <55386364+Marina-Garanina@users.noreply.github.com>
2019-10-14 07:25:45 +0300

    developed

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,22 +1,30 @@
 def calculate_frequences(text) :
+    if not isinstance(text, str):
+        return {}
+    work_text = ''
+    for el in str(text):
+        if el.isalpha() or el == ' ' or el == '\n':
+            work_text += el.lower()
+    words = work_text.split()
     dictionary = {}
-    text = "".join(word for word in text if word not in ('!', '.', ':', ';', ',', '“', '”', '"', '?', '?!', '@', '~', '$', '#', '№', '%', '*', '^'))
-    words = text.split()
     for key in words:
-        key = key.lower()
         if key in dictionary:
-            value = dictionary[key]
-            dictionary[key]=value+1
+            dictionary[key] += 1
         else:
-            dictionary[key]=1
+            dictionary[key] = 1
     return dictionary
 
 def filter_stop_words(dictionary, stopwords):
-    filtered_dictionary = dictionary
-    for key in list(filtered_dictionary.keys()):
-        if key in stopwords:
-            del filtered_dictionary[key]
-    return filtered_dictionary
+    if dictionary and stopwords is not None:
+        filtered_dictionary = dictionary.copy()
+        for key in dictionary:
+            if not isinstance(key, str):
+                del filtered_dictionary[key]
+        for word in stopwords:
+            if word in filtered_dictionary:
+                del filtered_dictionary[word]
+        return filtered_dictionary
+    return {}
 
 def get_top_n (dictionary, top_n):
     list_dictionary = list(dictionary.items())
@@ -29,3 +37,21 @@ def get_top_n (dictionary, top_n):
             index += 1
     toped_dictionary = tuple(list_dictionary[:top_n])
     return toped_dictionary
+
+def read_from_file (path_to_file, lines_limit):
+    file = open(path_to_file, 'r')
+    n = 0
+    text = ''
+    for line in file:
+        if n < lines_limit:
+            text += str(line)
+            n += 1
+    file.close()
+    return text
+
+def write_to_file (path_to_file, content):
+    file = open(path_to_file, 'w')
+    for el in content:
+        file.write(el)
+        file.write('\n')
+    file.close()
